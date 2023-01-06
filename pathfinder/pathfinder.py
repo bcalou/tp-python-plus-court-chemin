9537d627bb440eeea7c5f6098b6c783d2e0ee944
Roucool_PvP <lucastag77@gmail.com>
2023-01-06 14:45:06 +0100

    Remove du print

--- pathfinder/pathfinder.py
+++ pathfinder/pathfinder.py
@@ -15,7 +15,7 @@ class PathFinder:
         self.analyse_next_city(start, end)
         result: Path = self.get_path(start, end)
         self.reset()
-        
+
         return result
 
     def analyse_next_city(self, city: City, end: City, current_sum: float = 0) -> None:
@@ -43,8 +43,6 @@ class PathFinder:
     def get_path(self, start: City, end: City) -> Path:
         to_ret: Path = {"total": 0, "steps": []}
         current_city: City = end
-
-        print(self.villes)
         
         while current_city != start and self.villes[current_city]["previous"]:
             to_ret["steps"].insert(0, current_city)
