75eee9124cb58c79db9e445a6a76b91bc67e43b4
82L <16747176+82L@users.noreply.github.com>
2023-01-06 14:32:48 +0100

    City creation

---
+++ pathfinder/city.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class City(Enum):
+    BORDEAUX = "Bordeaux"
+    DIJON = "Dijon"
+    LILLE = "Lille"
+    RENNES = "Rennes"
+    NANTES = "Nantes"
+    PARIS = "Paris"
+    ORLEANS = "Orléans"
+    TOULOUSE = "Toulouse"
+    MARSEILLE = "Marseille"
+    LYON = "Lyon"
+    STRASBOURG = "Strasbourg"
+    ROUEN = "Rouen"
