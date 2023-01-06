44538904c2eedd3f98017f65586dde6f554f5bc9
Antoine <antoine.martin885@gmail.com>
2023-01-06 11:53:51 +0100

    Add city.py file

---
+++ pathfinder/city.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class City(Enum):
+    BORDEAUX = "Bordeaux"
+    DIJON = "Dijon"
+    LILLE = "Lille"
+    TOULOUSE = "Toulouse"
+    MARSEILLE = "Marseille"
+    LYON = "Lyon"
+    NANTES = "Nantes"
+    ORLEANS = "Orléans"
+    RENNES = "Rennes"
+    PARIS = "Paris"
+    STRASBOURG = "Strasbourg"
+    ROUEN = "Rouen"
