20e9ac1d150bd9e552cfbc95371341759b9b30be
Roucool_PvP <lucastag77@gmail.com>
2023-02-03 12:53:08 +0100

    Changement majeur de la logique

--- pathfinder/pathfinder.py
+++ pathfinder/pathfinder.py
@@ -9,8 +9,12 @@ class PathFinder:
         self.graph: Graph = graph
         self.villes: dict[City, Ville] = {}
         self.min_to_end: float = math.inf
+        self.to_visit: list[City] = []
 
     def get_shortest_path(self, start: City, end: City) -> Path:
+
+        self.to_visit.append(start)
+        self.villes[start] = {"previous": None, "cout": 0}
 
         self.analyse_next_city(start, end)
         result: Path = self.get_path(start, end)
@@ -18,29 +22,56 @@ class PathFinder:
 
         return result
 
+    # def analyse_next_city(self, city: City, end: City, current_sum: float = 0) -> None:
+    #     for neighbour in self.graph[city]:
+            
+    #         if current_sum > self.min_to_end:
+    #             return
+            
+    #         total_cost = current_sum + self.graph[city][neighbour]
+
+    #         if neighbour == end:
+    #             if total_cost < self.min_to_end:
+    #                 self.min_to_end = total_cost
+    #                 self.villes[neighbour] = {"previous": city, "cout": total_cost}
+    #             return
+
+    #         if (
+    #             neighbour not in self.villes or
+    #             self.villes[neighbour]["cout"] > total_cost
+    #         ):
+    #             self.villes[neighbour] = {"previous": city, "cout": total_cost}
+    #             self.analyse_next_city(neighbour, end, total_cost)
+
     def analyse_next_city(self, city: City, end: City, current_sum: float = 0) -> None:
-        for neighbour in self.graph[city]:
+        """Fonction principale de l'algo"""
+        while self.to_visit and not self.finished():
+            self.sort_visits()
+            city = self.to_visit.pop(0)
             
-            if current_sum > self.min_to_end:
-                return
-            
-            total_cost = current_sum + self.graph[city][neighbour]
+            for neighbour in self.graph[city]:
+                total_cost = self.villes[city]["cout"] + self.graph[city][neighbour]
 
-            if neighbour == end:
-                if total_cost < self.min_to_end:
-                    self.min_to_end = total_cost
+                if total_cost > self.min_to_end:
+                    continue
+                
+                if (
+                    neighbour not in self.villes or
+                    self.villes[neighbour]["cout"] > total_cost
+                ):
                     self.villes[neighbour] = {"previous": city, "cout": total_cost}
-                return
+                    self.to_visit.append(neighbour)
 
-            if (
-                neighbour not in self.villes or
-                self.villes[neighbour]["cout"] > total_cost
-            ):
-                self.villes[neighbour] = {"previous": city, "cout": total_cost}
-                self.analyse_next_city(neighbour, end, total_cost)
-            
+    def sort_visits(self) -> None:
+        """Algo de tri des villes à visiter, à modifier en fonction de l'algo"""
+        self.to_visit.sort(key=lambda city: self.villes[city]["cout"])
+
+    def finished(self) -> bool:
+        """Condition de fin de l'algo, à modifier en fonction de l'algo"""
+        return len(self.to_visit) == 0
 
     def get_path(self, start: City, end: City) -> Path:
+        """Lis le résultat de l'algo en partant de la fin"""
         to_ret: Path = {"total": 0, "steps": []}
         current_city: City = end
         
