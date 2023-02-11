a2c949ec82c66de09a679d9cafd48dbad6bbada8
Antoine <antoine.martin885@gmail.com>
2023-02-11 15:49:57 +0100

    Updated main with SPFA

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from pathfinder.astar import AStar
 from pathfinder.city import City
-from pathfinder.graphs import graph
+from pathfinder.graphs import graph, spfa_graph
 from pathfinder.heuristics import heuristics
 from pathfinder.pathfinder import PathFinder
+from pathfinder.spfa import SPFA
 
 pathfinder: PathFinder = PathFinder(graph)
 print(pathfinder.get_shortest_path(City.STRASBOURG, City.BORDEAUX))
 
 astar = AStar(graph, heuristics)
 print(astar.get_shortest_path(City.BORDEAUX, City.STRASBOURG))
+
+spfa = SPFA(spfa_graph)
+print(spfa.get_shortest_path(City.BORDEAUX, City.STRASBOURG))
