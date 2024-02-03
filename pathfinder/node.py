90709d12a9a8f8be91de15de52768b2683063d3a
Hugo <53018953+YannoteX@users.noreply.github.com>
2024-02-03 18:20:36 +0100

    update

--- pathfinder/node.py
+++ pathfinder/node.py
@@ -1,10 +1,24 @@
+from pathfinder.city import City
+
+
 class Node:
 
-    def __init__(self) -> None:
+    def __init__(self, new_city: City) -> None:
+        self.__name: City = new_city
         self.__distance: float = float("inf")
+        self.__previous_node: City = new_city
 
     def get_distance(self) -> float:
         return self.__distance
 
+    def get_name(self) -> City:
+        return self.__name
+
+    def get_previous_node(self) -> str:
+        return self.__previous_node
+
     def set_distance(self, new_distance: float) -> None:
         self.__distance = new_distance
+
+    def set_previous_node(self, new_city: City):
+        self.__previous_node = new_city
--- pathfinder/pathfinder.py
+++ pathfinder/pathfinder.py
@@ -1,6 +1,7 @@
 from pathfinder.city import City
 from pathfinder.types import Path
 from pathfinder.graphs import Graph
+from pathfinder.node import Node
 
 
 class Pathfinder:
@@ -13,6 +14,11 @@ class Pathfinder:
         }
 
         self.__inner_graph: Graph = graph
+
+        self.__nodes: list[Node] = []
+
+        for i in graph.keys():
+            self.__nodes.append(Node(i.value))
 
         self.__travelled_cities: list[City] = []
 
@@ -32,32 +38,3 @@ class Pathfinder:
             self.__travelled_cities.append(last_step)
 
         return self.__path
-
-    def __find_nearest_city(self, start_city: City) -> None:
-
-        node: list[dict[City, float]] = self.__inner_graph[start_city]
-        new_step: City = list(node.keys())[0]
-        distance: int = node[new_step]
-
-        for i in node:
-            new_distance: int = node[i]
-
-            if not self.__has_been_travelled(i):
-
-                if (self.__has_been_travelled(new_step) or
-                        not self.__has_been_travelled(new_step) and
-                        new_distance < distance):
-
-                    distance = new_distance
-                    new_step = i
-
-        self.__path["total"] += distance
-        self.__path["steps"].append(new_step)
-
-    def __has_been_travelled(self, city: City):
-
-        for i in self.__travelled_cities:
-            if i == city:
-                return True
-
-        return False
