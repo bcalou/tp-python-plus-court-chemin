919c2cc3a215a7260b4def8eb4bb1b82afd62e8b
mmaury2727 <120041871+mmaury2727@users.noreply.github.com>
2024-02-18 20:06:08 +0100

    mouai

--- pathfinder/astar.py
+++ pathfinder/astar.py
@@ -44,14 +44,7 @@ class AStarPathfinder(Pathfinder):
         return {'total': distances[path[-1]], 'steps': path}
 
     def heuristic(self, city: City, end: City) -> float:
-        # Calcul de la distance à vol d'oiseau entre la ville et la destination
-        # Ici, je suppose que la vitesse du cycliste est de 16 km/h
-        # Vous pouvez ajuster cette valeur en fonction de vos besoins
-        # Assurez-vous que la distance est exprimée en heures pour correspondre au temps
         return self.distance(city, end) / 16
 
     def distance(self, city1: City, city2: City) -> float:
-        # Implémentez le calcul de la distance à vol d'oiseau entre deux villes ici
-        # Vous pouvez utiliser la formule de la distance euclidienne entre deux points géographiques
-        # Assurez-vous que la distance est exprimée en heures pour correspondre au temps
         pass
